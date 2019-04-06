5b83d76c657b09b1ad0de31756ccf3f12cffc48f
jdlrobson <jdlrobson@gmail.com>
2019-04-05 22:19:45 -0700

    Add template to book page

--- generate-vocab.js
+++ generate-vocab.js
@@ -261,6 +261,39 @@ function generateAboutUs() {
     );
 }
 
+function generateBookPage() {
+    const bookText = fs.readFileSync(`littledailydose.wiki/Book.md`).toString();
+    const articlehtml = `<img class="bookcover" src="Header_1.jpg" width="2100" alt="A Little Daily Dose" />
+    <section>
+        <h1>Pre-order Special</h1>
+        <strong>USD: $28.80</strong>
+        <form class="paypal-form" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="ADGZNJEJ9GN4C">
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+        </form>
+        ${marked( bookText )}
+    </section>`
+    // make index.html
+    fs.writeFile(
+        `public/index.html`,
+        template.render( {
+            noHeader: true,
+            articlehtml,
+            noHeading: true,
+            isStoryPage: true,
+            title: 'Buy the book',
+            strokes: 'buy',
+            definitions: [],
+            personalNote: false
+        } ),
+        {
+            encoding: 'utf8'
+        }
+    );
+};
+
 if ( process.argv[2] ) {
     generatePage( process.argv[2] );
 } else {
@@ -268,5 +301,6 @@ if ( process.argv[2] ) {
     generateToc();
     generateIndex();
     generateAboutUs();
+    generateBookPage();
 }
 
