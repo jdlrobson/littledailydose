bf34fa7b28dcebe81ddb8e7b2436470d0e73cc48
jdlrobson <jdlrobson@gmail.com>
2019-04-06 12:36:36 -0700

    Update book page

--- generate-vocab.js
+++ generate-vocab.js
@@ -264,24 +264,22 @@ function generateAboutUs() {
 function generateBookPage() {
     const bookText = fs.readFileSync(`littledailydose.wiki/Book.md`).toString();
     const articlehtml = `<img class="bookcover" src="Header_1.jpg" width="2100" alt="A Little Daily Dose" />
-    <section>
-        <h1>Pre-order Special</h1>
-        <strong>USD: $28.80</strong>
+    <section class="book-buy">
+        ${marked( bookText )}
         <form class="paypal-form" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <h2>Pre-order Special</h2>
+            <strong>USD: $28.80</strong>
             <input type="hidden" name="cmd" value="_s-xclick">
             <input type="hidden" name="hosted_button_id" value="ADGZNJEJ9GN4C">
             <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
             <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
         </form>
-        ${marked( bookText )}
     </section>`
     // make index.html
     fs.writeFile(
         `public/index.html`,
         template.render( {
-            noHeader: true,
             articlehtml,
-            noHeading: true,
             isStoryPage: true,
             title: 'Buy the book',
             strokes: 'buy',
